d1f3c9546747f1112de822a69fc0f773956da0bb
Nicolas Filliol <nicolas.filliol@icloud.com>
2022-11-25 11:26:30 -0500

    added function for finetuning

--- pruning.py
+++ pruning.py
@@ -4,6 +4,7 @@ import copy
 from tqdm.auto import tqdm
 from pruning.fine_grained_pruning import FineGrainedPruner
 from evaluate import evaluate
+from finetune import finetune
 from SuperGlue.models.matching import Matching
 import matplotlib.pyplot as plt
 import math
@@ -81,38 +82,45 @@ def main():
     config = {
         'superpoint': {
             'nms_radius': 4,
-            'keypoint_threshold': 0.005,
-            'max_keypoints': -1
+            'keypoint_threshold': 0.0,
+            'max_keypoints': 512,
+            'remove_borders': 4
         },
         'superglue': {
             'weights_path': "/Users/nfilliol/Desktop/ETH/MIT_HS22/TinyML/efficient-feature-matching/SuperGlue/models/weights/superglue_cocohomo.pt",
-            'sinkhorn_iterations': 20,
+            'sinkhorn_iterations': 100,
             'match_threshold': 0.2,
+            'num_layers': 9,
+            'use_layernorm': False,
+            'bin_value': 1.0,
+            'pos_loss_weight': 0.45,
+            'neg_loss_weight': 1.0,
+            'restore_path': ""
         }
     }
 
     device = 'cpu'
     matching = Matching(config).eval().to(device)
 
-     # Randomly sample a subset of parameter sets for tests
-    param_sets = [(name, param) for (name, param) in matching.named_parameters() if param.dim() > 1 and "superglue" in name]
-    sample_param_sets = random.sample(param_sets, 12)
+    # # Randomly sample a subset of parameter sets for tests
+    # param_sets = [(name, param) for (name, param) in matching.named_parameters() if param.dim() > 1 and "superglue" in name]
+    # sample_param_sets = random.sample(param_sets, 12)
 
-    # Profile and evaluate dense model
-    helper.profile_matching_model(matching, count_nonzero_only=False)
-    helper.plot_weight_distribution(sample_param_sets, out_path="weight_distribution_dense.png", count_nonzero_only=False)
+    # # Profile and evaluate dense model
+    # helper.profile_matching_model(matching, count_nonzero_only=False)
+    # helper.plot_weight_distribution(sample_param_sets, out_path="weight_distribution_dense.png", count_nonzero_only=False)
 
-    dense_results = evaluate(matching)
-    print(f"Dense Results = {dense_results}")
+    # dense_results = evaluate(matching)
+    # print(f"Dense Results = {dense_results}")
     
-    # Print Model Structure and plot distribution of weights
-    helper.print_model_structure(matching.superglue)
+    # # Print Model Structure and plot distribution of weights
+    # helper.print_model_structure(matching.superglue)
 
-    # Fine-grained Pruning (magnitude based)
-    # Sensitivity Scan
-    names, sparsities, precisions, recalls = sensitivity_scan(matching, sample_param_sets, scan_step=0.2, scan_start=0.5, scan_end=1.0)
-    plot_sensitivity_scan(names, sparsities, precisions, dense_results["precision"], metric="precision")
-    plot_sensitivity_scan(names, sparsities, recalls, dense_results["recall"], metric="recall")
+    # # Fine-grained Pruning (magnitude based)
+    # # Sensitivity Scan
+    # names, sparsities, precisions, recalls = sensitivity_scan(matching, sample_param_sets, scan_step=0.2, scan_start=0.5, scan_end=1.0)
+    # plot_sensitivity_scan(names, sparsities, precisions, dense_results["precision"], metric="precision")
+    # plot_sensitivity_scan(names, sparsities, recalls, dense_results["recall"], metric="recall")
 
     # Prune
     sparsity_for_layer_type = {
@@ -131,10 +139,12 @@ def main():
                 sparsity_dict[name] = 0
 
     pruner = FineGrainedPruner(matching, sparsity_dict)
-    helper.profile_matching_model(pruner.model, count_nonzero_only=True)
-    helper.plot_weight_distribution(sample_param_sets, out_path="weight_distribution_pruned.png", count_nonzero_only=True)
-    pruned_results = evaluate(pruner.model, max_evaluation_points=-1)
-    print(f"Pruned Results = {pruned_results}")
+    # helper.profile_matching_model(pruner.model, count_nonzero_only=True)
+    # helper.plot_weight_distribution(sample_param_sets, out_path="weight_distribution_pruned.png", count_nonzero_only=True)
+    # pruned_results = evaluate(pruner.model, max_evaluation_points=-1)
+    # print(f"Pruned Results = {pruned_results}")
+
+    finetune(pruner.model, max_epochs=1)
 
 
 if __name__ == "__main__":
